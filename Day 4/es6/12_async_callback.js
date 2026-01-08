39fe6d56d0d5b9c2e234492e934d6d741dec15d5
SnowvyTH <supanat617@gmail.com>
2026-01-08 07:57:25 +0700

    Day 4 - Update

---
+++ Day 4/es6/12_async_callback.js
@@ -0,0 +1,53 @@
+// Synchronous & Asyncronous Programming in Javascript
+// function doA() {
+//     setTimeout(function() {
+//         console.log("This is 1st say message")
+//     }, 2000)
+// }
+// function doB() {
+//     console.log("This is 2nd say message")
+// }
+
+// doA()
+// doB()
+
+function doA(callback) {
+    console.log("Loading...")
+    setTimeout(function() {
+        console.log("This is 1st say message")
+        callback()
+    }, 2000)
+}
+
+function doB() {
+    console.log("This is 2nd say message")
+}
+
+doA(function(){
+    doB()
+})
+
+function loadNews(cb){
+    console.log("Loading news...")
+    setTimeout(() =>{
+        cb(
+            [
+                {id: 1, title: "a"},
+                {id: 2, title: "b"},
+                {id: 3, title: "c"},
+                {id: 4, title: "d"},
+                {id: 5, title: "e"},
+                {id: 6, title: "f"},
+                {id: 7, title: "g"},
+                {id: 8, title: "h"},
+                {id: 9, title: "i"},
+            ]
+        )
+    }, 2000)
+}
+
+loadNews(function(result){
+    console.log(result)
+    console.log("Load Completed")
+})
+
