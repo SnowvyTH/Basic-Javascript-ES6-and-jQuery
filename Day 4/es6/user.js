95a7f9cb763835b45b30cfcd8132f68c5f18881b
SnowvyTH <supanat617@gmail.com>
2026-01-10 18:15:36 +0700

    Finish Learning

--- Day 4/es6/user.js
+++
@@ -1,9 +0,0 @@
-export default class User { // มีการเติม default ไปหลัง
-    constructor(name) {
-        this.name = name
-    }
-
-    sayHi() {
-        console.log("Hi " + this.name)
-    }
-}
--- Day 5/fundamental/15_getElement.js
+++
@@ -1,8 +0,0 @@
-// การเข้าถึง Tag ด้วย ID
-document.getElementById("result").innerHTML = "Hello World!";
-document.getElementById("result").style.backgroundColor = "yellow";
-
-document.getElementsByTagName("h1")[0].style.backgroundColor = "lightblue";
-document.getElementsByTagName("h1")[1].style.backgroundColor = "lightgreen";
-
-// การเข้าถึง Tag ด้วย Class
